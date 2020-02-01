9b5ca852243b27171f7a099db8ca16efbe3d6f1f
thanhnd59 <thanhnd59@viettel.com.vn>
2020-02-01 13:12:54 +0700

    Fix files

--- public/app/core/components/Footer/Footer.tsx
+++ public/app/core/components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import { Tooltip } from '@grafana/ui';
 
 interface Props {
   appName: string;
@@ -16,41 +15,11 @@ export const Footer: FC<Props> = React.memo(
         <div className="text-center">
           <ul>
             <li>
-              <a href="http://docs.grafana.org" target="_blank" rel="noopener">
-                <i className="fa fa-file-code-o" /> Docs
+              <a href="http://erp.htaviet.com:9397/" target="_blank">
+                <i className="fa fa-support"></i>
+                ERP HtaViet
               </a>
             </li>
-            <li>
-              <a
-                href="https://grafana.com/products/enterprise/?utm_source=grafana_footer"
-                target="_blank"
-                rel="noopener"
-              >
-                <i className="fa fa-support" /> Support & Enterprise
-              </a>
-            </li>
-            <li>
-              <a href="https://community.grafana.com/" target="_blank" rel="noopener">
-                <i className="fa fa-comments-o" /> Community
-              </a>
-            </li>
-            <li>
-              <a href="https://grafana.com" target="_blank" rel="noopener">
-                {appName}
-              </a>{' '}
-              <span>
-                v{buildVersion} (commit: {buildCommit})
-              </span>
-            </li>
-            {newGrafanaVersionExists && (
-              <li>
-                <Tooltip placement="auto" content={newGrafanaVersion}>
-                  <a href="https://grafana.com/get" target="_blank" rel="noopener">
-                    New version available!
-                  </a>
-                </Tooltip>
-              </li>
-            )}
           </ul>
         </div>
       </footer>
